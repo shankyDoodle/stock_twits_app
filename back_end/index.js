c42b166832ed4c01a4ad63d3134fd9d1befec7a9
skaldat1 <skaldat1@binghamton.edu>
2020-05-15 18:34:52 -0400

    backend message get call implemented

--- back_end/index.js
+++ back_end/index.js
@@ -2,9 +2,9 @@
 
 const express = require('express')
 const cors = require('cors');
-const fs = require('fs')
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
+const axios = require('axios');
 
 const OK = 200;
 const CREATED = 201;
@@ -19,6 +19,12 @@ myData.prepareData()
 
 const port = 9091
 
+
+const dotenv = require('dotenv');
+dotenv.config();
+const STOCK_TOKEN = process.env.STOCK_TOKEN
+
+
 const app = express()
 app.use(bodyParser.json());
 app.use(bodyParser.raw());
@@ -28,7 +34,33 @@ app.use(cookieParser());
 app.use(cors())
 
 app.get('/symbolList', (req, res) => res.send(myData.symbolList));
-app.get('/symbolData', (req, res) => res.send("myData.symbolData"));
+app.get('/symbolData', (req, res) => {
+    let stringQuery = req.query;
+    let symbols = stringQuery.symbols
+
+    let baseURL = "https://api.stocktwits.com/api/2/streams/symbols.json?";
+    let stockTokenQuery = "access_token=" + STOCK_TOKEN;
+    let symbolsQuery = "&symbols=" + symbols;
+    axios.get(baseURL + stockTokenQuery + symbolsQuery)
+        .then(innerRes => {
+            let data = innerRes.data;
+            let oRetData = {};
+            oRetData.cursor = data.cursor;
+            oRetData.message = data.messages.map(oM => {
+                return {
+                    id: oM.id,
+                    body: oM.body,
+                    created_at: oM.created_at,
+                    symbols: oM.symbols
+                }
+            })
+            res.statusCode = OK;
+            res.send(oRetData)
+        }).catch(e => {
+        res.statusCode = BAD_REQUEST;
+        res.send(e)
+    });
+});
 
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
---
+++ back_end/mockdata/message-data.js
@@ -0,0 +1,443 @@
+module.exports = [
+    {
+        "id": 16,
+        "body": "more ideas about $TF_F",
+        "created_at": "2012-10-08 21:39:15 UTC",
+        "user": {
+            "id": 2,
+            "username": "ppearlman",
+            "name": "Phil Pearlman",
+            "avatar_url": "http://avatars.stocktwits.com/images/default_avatar_thumb.jpg",
+            "avatar_url_ssl": "https://s3.amazonaws.com/st-avatars/images/default_avatar_thumb.jpg",
+            "identity": "User",
+            "classification": [
+
+            ],
+            "followers": 0,
+            "following": 0,
+            "ideas": 0,
+            "following_stocks": 0,
+            "bio": "Psychologist, Hedgie, entrepeneur, seed investor, stocktwits ftw!",
+            "website_url": "http://philpearlman.com/",
+            "trading_strategy": {
+                "assets_frequently_traded": [
+                    "Equities"
+                ],
+                "approach": "Growth",
+                "holding_period": "Long Term Investor",
+                "experience": "Novice"
+            }
+        },
+        "source": {
+            "id": 1,
+            "title": "Stocktwits",
+            "url": "http://stocktwits.com"
+        },
+        "symbols": [
+            {
+                "id": 32,
+                "symbol": "TF_F",
+                "title": "Russell 2000 Index Mini Futures",
+                "exchange": "ICE",
+                "sector": null,
+                "industry": null,
+                "trending": false
+            }
+        ],
+        "entities": {
+            "sentiment": null
+        }
+    },
+    {
+        "id": 17,
+        "body": "something about $ES_F",
+        "created_at": "2012-10-08 21:39:16 UTC",
+        "user": {
+            "id": 2,
+            "username": "ppearlman",
+            "name": "Phil Pearlman",
+            "avatar_url": "http://avatars.stocktwits.com/images/default_avatar_thumb.jpg",
+            "avatar_url_ssl": "https://s3.amazonaws.com/st-avatars/images/default_avatar_thumb.jpg",
+            "identity": "User",
+            "classification": [
+
+            ],
+            "followers": 0,
+            "following": 0,
+            "ideas": 0,
+            "following_stocks": 0,
+            "bio": "Psychologist, Hedgie, entrepeneur, seed investor, stocktwits ftw!",
+            "website_url": "http://philpearlman.com/",
+            "trading_strategy": {
+                "assets_frequently_traded": [
+                    "Equities"
+                ],
+                "approach": "Growth",
+                "holding_period": "Long Term Investor",
+                "experience": "Novice"
+            }
+        },
+        "source": {
+            "id": 1,
+            "title": "Stocktwits",
+            "url": "http://stocktwits.com"
+        },
+        "symbols": [
+            {
+                "id": 30,
+                "symbol": "ES_F",
+                "title": "E-Mini S&P 500 Futures",
+                "exchange": "CME",
+                "sector": null,
+                "industry": null,
+                "trending": false
+            }
+        ],
+        "entities": {
+            "sentiment": null
+        }
+    },
+    {
+        "id": 18,
+        "body": "I bought $NQ_F",
+        "created_at": "2012-10-08 21:39:16 UTC",
+        "user": {
+            "id": 2,
+            "username": "ppearlman",
+            "name": "Phil Pearlman",
+            "avatar_url": "http://avatars.stocktwits.com/images/default_avatar_thumb.jpg",
+            "avatar_url_ssl": "https://s3.amazonaws.com/st-avatars/images/default_avatar_thumb.jpg",
+            "identity": "User",
+            "classification": [
+
+            ],
+            "followers": 0,
+            "following": 0,
+            "ideas": 0,
+            "following_stocks": 0,
+            "bio": "Psychologist, Hedgie, entrepeneur, seed investor, stocktwits ftw!",
+            "website_url": "http://philpearlman.com/",
+            "trading_strategy": {
+                "assets_frequently_traded": [
+                    "Equities"
+                ],
+                "approach": "Growth",
+                "holding_period": "Long Term Investor",
+                "experience": "Novice"
+            }
+        },
+        "source": {
+            "id": 1,
+            "title": "Stocktwits",
+            "url": "http://stocktwits.com"
+        },
+        "symbols": [
+            {
+                "id": 31,
+                "symbol": "NQ_F",
+                "title": "E-Mini NASDAQ 100 Futures",
+                "exchange": "CME",
+                "sector": null,
+                "industry": null,
+                "trending": false
+            }
+        ],
+        "entities": {
+            "sentiment": null
+        }
+    },
+    {
+        "id": 19,
+        "body": "more ideas about $TF_F",
+        "created_at": "2012-10-08 21:39:15 UTC",
+        "user": {
+            "id": 2,
+            "username": "ppearlman",
+            "name": "Phil Pearlman",
+            "avatar_url": "http://avatars.stocktwits.com/images/default_avatar_thumb.jpg",
+            "avatar_url_ssl": "https://s3.amazonaws.com/st-avatars/images/default_avatar_thumb.jpg",
+            "identity": "User",
+            "classification": [
+
+            ],
+            "followers": 0,
+            "following": 0,
+            "ideas": 0,
+            "following_stocks": 0,
+            "bio": "Psychologist, Hedgie, entrepeneur, seed investor, stocktwits ftw!",
+            "website_url": "http://philpearlman.com/",
+            "trading_strategy": {
+                "assets_frequently_traded": [
+                    "Equities"
+                ],
+                "approach": "Growth",
+                "holding_period": "Long Term Investor",
+                "experience": "Novice"
+            }
+        },
+        "source": {
+            "id": 1,
+            "title": "Stocktwits",
+            "url": "http://stocktwits.com"
+        },
+        "symbols": [
+            {
+                "id": 32,
+                "symbol": "TF_F",
+                "title": "Russell 2000 Index Mini Futures",
+                "exchange": "ICE",
+                "sector": null,
+                "industry": null,
+                "trending": false
+            }
+        ],
+        "entities": {
+            "sentiment": null
+        }
+    },
+    {
+        "id": 20,
+        "body": "something about $ES_F",
+        "created_at": "2012-10-08 21:39:16 UTC",
+        "user": {
+            "id": 2,
+            "username": "ppearlman",
+            "name": "Phil Pearlman",
+            "avatar_url": "http://avatars.stocktwits.com/images/default_avatar_thumb.jpg",
+            "avatar_url_ssl": "https://s3.amazonaws.com/st-avatars/images/default_avatar_thumb.jpg",
+            "identity": "User",
+            "classification": [
+
+            ],
+            "followers": 0,
+            "following": 0,
+            "ideas": 0,
+            "following_stocks": 0,
+            "bio": "Psychologist, Hedgie, entrepeneur, seed investor, stocktwits ftw!",
+            "website_url": "http://philpearlman.com/",
+            "trading_strategy": {
+                "assets_frequently_traded": [
+                    "Equities"
+                ],
+                "approach": "Growth",
+                "holding_period": "Long Term Investor",
+                "experience": "Novice"
+            }
+        },
+        "source": {
+            "id": 1,
+            "title": "Stocktwits",
+            "url": "http://stocktwits.com"
+        },
+        "symbols": [
+            {
+                "id": 30,
+                "symbol": "ES_F",
+                "title": "E-Mini S&P 500 Futures",
+                "exchange": "CME",
+                "sector": null,
+                "industry": null,
+                "trending": false
+            }
+        ],
+        "entities": {
+            "sentiment": null
+        }
+    },
+    {
+        "id": 21,
+        "body": "I bought $NQ_F",
+        "created_at": "2012-10-08 21:39:16 UTC",
+        "user": {
+            "id": 2,
+            "username": "ppearlman",
+            "name": "Phil Pearlman",
+            "avatar_url": "http://avatars.stocktwits.com/images/default_avatar_thumb.jpg",
+            "avatar_url_ssl": "https://s3.amazonaws.com/st-avatars/images/default_avatar_thumb.jpg",
+            "identity": "User",
+            "classification": [
+
+            ],
+            "followers": 0,
+            "following": 0,
+            "ideas": 0,
+            "following_stocks": 0,
+            "bio": "Psychologist, Hedgie, entrepeneur, seed investor, stocktwits ftw!",
+            "website_url": "http://philpearlman.com/",
+            "trading_strategy": {
+                "assets_frequently_traded": [
+                    "Equities"
+                ],
+                "approach": "Growth",
+                "holding_period": "Long Term Investor",
+                "experience": "Novice"
+            }
+        },
+        "source": {
+            "id": 1,
+            "title": "Stocktwits",
+            "url": "http://stocktwits.com"
+        },
+        "symbols": [
+            {
+                "id": 31,
+                "symbol": "NQ_F",
+                "title": "E-Mini NASDAQ 100 Futures",
+                "exchange": "CME",
+                "sector": null,
+                "industry": null,
+                "trending": false
+            }
+        ],
+        "entities": {
+            "sentiment": null
+        }
+    },
+    {
+        "id": 22,
+        "body": "more ideas about $TF_F",
+        "created_at": "2012-10-08 21:39:15 UTC",
+        "user": {
+            "id": 2,
+            "username": "ppearlman",
+            "name": "Phil Pearlman",
+            "avatar_url": "http://avatars.stocktwits.com/images/default_avatar_thumb.jpg",
+            "avatar_url_ssl": "https://s3.amazonaws.com/st-avatars/images/default_avatar_thumb.jpg",
+            "identity": "User",
+            "classification": [
+
+            ],
+            "followers": 0,
+            "following": 0,
+            "ideas": 0,
+            "following_stocks": 0,
+            "bio": "Psychologist, Hedgie, entrepeneur, seed investor, stocktwits ftw!",
+            "website_url": "http://philpearlman.com/",
+            "trading_strategy": {
+                "assets_frequently_traded": [
+                    "Equities"
+                ],
+                "approach": "Growth",
+                "holding_period": "Long Term Investor",
+                "experience": "Novice"
+            }
+        },
+        "source": {
+            "id": 1,
+            "title": "Stocktwits",
+            "url": "http://stocktwits.com"
+        },
+        "symbols": [
+            {
+                "id": 32,
+                "symbol": "TF_F",
+                "title": "Russell 2000 Index Mini Futures",
+                "exchange": "ICE",
+                "sector": null,
+                "industry": null,
+                "trending": false
+            }
+        ],
+        "entities": {
+            "sentiment": null
+        }
+    },
+    {
+        "id": 23,
+        "body": "something about $ES_F",
+        "created_at": "2012-10-08 21:39:16 UTC",
+        "user": {
+            "id": 2,
+            "username": "ppearlman",
+            "name": "Phil Pearlman",
+            "avatar_url": "http://avatars.stocktwits.com/images/default_avatar_thumb.jpg",
+            "avatar_url_ssl": "https://s3.amazonaws.com/st-avatars/images/default_avatar_thumb.jpg",
+            "identity": "User",
+            "classification": [
+
+            ],
+            "followers": 0,
+            "following": 0,
+            "ideas": 0,
+            "following_stocks": 0,
+            "bio": "Psychologist, Hedgie, entrepeneur, seed investor, stocktwits ftw!",
+            "website_url": "http://philpearlman.com/",
+            "trading_strategy": {
+                "assets_frequently_traded": [
+                    "Equities"
+                ],
+                "approach": "Growth",
+                "holding_period": "Long Term Investor",
+                "experience": "Novice"
+            }
+        },
+        "source": {
+            "id": 1,
+            "title": "Stocktwits",
+            "url": "http://stocktwits.com"
+        },
+        "symbols": [
+            {
+                "id": 30,
+                "symbol": "ES_F",
+                "title": "E-Mini S&P 500 Futures",
+                "exchange": "CME",
+                "sector": null,
+                "industry": null,
+                "trending": false
+            }
+        ],
+        "entities": {
+            "sentiment": null
+        }
+    },
+    {
+        "id": 24,
+        "body": "I bought $NQ_F",
+        "created_at": "2012-10-08 21:39:16 UTC",
+        "user": {
+            "id": 2,
+            "username": "ppearlman",
+            "name": "Phil Pearlman",
+            "avatar_url": "http://avatars.stocktwits.com/images/default_avatar_thumb.jpg",
+            "avatar_url_ssl": "https://s3.amazonaws.com/st-avatars/images/default_avatar_thumb.jpg",
+            "identity": "User",
+            "classification": [
+
+            ],
+            "followers": 0,
+            "following": 0,
+            "ideas": 0,
+            "following_stocks": 0,
+            "bio": "Psychologist, Hedgie, entrepeneur, seed investor, stocktwits ftw!",
+            "website_url": "http://philpearlman.com/",
+            "trading_strategy": {
+                "assets_frequently_traded": [
+                    "Equities"
+                ],
+                "approach": "Growth",
+                "holding_period": "Long Term Investor",
+                "experience": "Novice"
+            }
+        },
+        "source": {
+            "id": 1,
+            "title": "Stocktwits",
+            "url": "http://stocktwits.com"
+        },
+        "symbols": [
+            {
+                "id": 31,
+                "symbol": "NQ_F",
+                "title": "E-Mini NASDAQ 100 Futures",
+                "exchange": "CME",
+                "sector": null,
+                "industry": null,
+                "trending": false
+            }
+        ],
+        "entities": {
+            "sentiment": null
+        }
+    }
+]
